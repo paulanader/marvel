9440128da5d9e3f5960a1aab0dda6b364dbd6630
Paula Nader <paulampaan@gmail.com>
2022-04-25 11:36:48 -0300

    CSS homeCard

--- src/components/HomeCard/style.ts
+++ src/components/HomeCard/style.ts
@@ -1,9 +1,16 @@
 import styled from "styled-components";
 
 export const Base = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
   align-items: center;
   justify-content: space-between;
+  font-size: 1rem;
   color: var(--red);
+  background: var(--black);
+  border: 0;
+
+  padding: 0 2rem;
 
   a {
     color: var(--black);
